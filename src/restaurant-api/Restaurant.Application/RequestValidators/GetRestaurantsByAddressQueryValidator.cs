c0132e6223938bad5abf44f9e82398e88d95ca04
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2022-11-27 16:44:18 -0300

    [restaurants]: fix get restaurants by address validation

--- src/restaurant-api/Restaurant.Application/RequestValidators/GetRestaurantsByAddressQueryValidator.cs
+++ src/restaurant-api/Restaurant.Application/RequestValidators/GetRestaurantsByAddressQueryValidator.cs
@@ -8,11 +8,23 @@
 
             RuleFor(r => r.Rows).NotNull().GreaterThan(0);
 
-            RuleFor(r => r.Neighborhood).NotNull().NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.City) && string.IsNullOrWhiteSpace(x.Zone));
+            RuleFor(r => r.Neighborhood).NotNull()
+                                        .Unless(x => !string.IsNullOrWhiteSpace(x.City) || !string.IsNullOrWhiteSpace(x.Zone))
+                                        .WithMessage(r => $"'{nameof(r.Neighborhood)}' must not be null.")
+                                        .NotEmpty()
+                                        .Unless(x => !string.IsNullOrWhiteSpace(x.City) || !string.IsNullOrWhiteSpace(x.Zone));
            
-            RuleFor(r => r.City).NotNull().NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.Neighborhood) && string.IsNullOrWhiteSpace(x.Zone));
+            RuleFor(r => r.City).NotNull()
+                                .Unless(x => !string.IsNullOrWhiteSpace(x.Neighborhood) || !string.IsNullOrWhiteSpace(x.Zone))
+                                .WithMessage(r => $"'{nameof(r.City)}' must not be null.")
+                                .NotEmpty()
+                                .Unless(x => !string.IsNullOrWhiteSpace(x.Neighborhood) || !string.IsNullOrWhiteSpace(x.Zone));
             
-            RuleFor(r => r.Zone).NotNull().NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.City) && string.IsNullOrWhiteSpace(x.Neighborhood));
+            RuleFor(r => r.Zone).NotNull()
+                                .Unless(x => !string.IsNullOrWhiteSpace(x.City) || !string.IsNullOrWhiteSpace(x.Neighborhood))
+                                .WithMessage(r => $"'{nameof(r.Zone)}' must not be null.")
+                                .NotEmpty()
+                                .Unless(x => !string.IsNullOrWhiteSpace(x.City) || !string.IsNullOrWhiteSpace(x.Neighborhood));
         }
     }
 }
