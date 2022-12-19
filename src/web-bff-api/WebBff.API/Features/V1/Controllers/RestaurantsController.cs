7ebf4ebcd44a3b757879c02231db0fcec0084ee5
Enzo Thomazi Lucas <enzotlucas31@gmail.com>
2022-12-19 12:32:47 -0300

    creating bff

---
+++ src/web-bff-api/WebBff.API/Features/V1/Controllers/RestaurantsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBff.API.Features.V1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/restaurants")]
+    [Produces("application/json")]
+    public class RestaurantsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await Task.Run(() => { return Ok(); });
+        }
+    }
+}
